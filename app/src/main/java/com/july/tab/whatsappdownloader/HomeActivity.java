859070fdd300a760618a92818dc326999a617c61
tabrez7311 <tabrezshaikh7311@gmail.com>
2017-07-09 16:51:30 +0530

    Whatsapp Status Downloader

---
+++ app/src/main/java/com/july/tab/whatsappdownloader/HomeActivity.java
@@ -0,0 +1,63 @@
+package com.july.tab.whatsappdownloader;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.july.tab.whatsappdownloader.adapter.RecyclerViewMediaAdapter;
+import com.july.tab.whatsappdownloader.base.BaseActivity;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class HomeActivity extends BaseActivity {
+
+    private static final String WHATSAPP_STATUSES_LOCATION = "/storage/emulated/legacy/WhatsApp/Media/.Statuses";
+    private RecyclerView mRecyclerViewMediaList;
+    private LinearLayoutManager mLinearLayoutManager;
+    public static final String TAG = "Home";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_home);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        mRecyclerViewMediaList = (RecyclerView) findViewById(R.id.recyclerViewMedia);
+        mLinearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerViewMediaList.setLayoutManager(mLinearLayoutManager);
+        RecyclerViewMediaAdapter recyclerViewMediaAdapter = new RecyclerViewMediaAdapter(this.getListFiles(new File(WHATSAPP_STATUSES_LOCATION)), HomeActivity.this);
+        mRecyclerViewMediaList.setAdapter(recyclerViewMediaAdapter);
+    }
+
+    /**
+     * get all the files in specified directory
+     *
+     * @param parentDir
+     * @return
+     */
+    private ArrayList<File> getListFiles(File parentDir) {
+        ArrayList<File> inFiles = new ArrayList<File>();
+        File[] files;
+        files = parentDir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+
+                if (file.getName().endsWith(".jpg") ||
+                        file.getName().endsWith(".gif") ||
+                        file.getName().endsWith(".mp4")) {
+                    if (!inFiles.contains(file))
+                        inFiles.add(file);
+                }
+            }
+        }
+        return inFiles;
+    }
+
+
+}
